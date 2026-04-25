5f6259980a9813662c6423467a3c2cfcb8228bde
coyoteatento-debug <coyoteatento@gmail.com>
2026-04-25 15:43:53 -0600

    Update gps_service.dart

--- lib/core/services/gps_service.dart
+++ lib/core/services/gps_service.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:geolocator/geolocator.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter/foundation.dart';
 
 class GpsService {
   const GpsService();
