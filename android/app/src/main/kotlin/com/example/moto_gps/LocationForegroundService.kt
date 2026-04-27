ff8866363fbbb1f5ac7b36120110bde34c80af60
coyoteatento-debug <coyoteatento@gmail.com>
2026-04-26 20:39:57 -0600

    Update LocationForegroundService.kt

--- android/app/src/main/kotlin/com/example/moto_gps/LocationForegroundService.kt
+++ android/app/src/main/kotlin/com/example/moto_gps/LocationForegroundService.kt
@@ -3,11 +3,15 @@ package com.example.moto_gps
 import android.app.*
 import android.content.Intent
 import android.content.pm.ServiceInfo
-import android.location.Location
 import android.os.*
 import android.util.Log
 import androidx.core.app.NotificationCompat
-import com.google.android.gms.location.*
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 
 class LocationForegroundService : Service() {
 
