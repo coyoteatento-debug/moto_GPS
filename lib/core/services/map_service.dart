8ab2f62e3c0421c7d813695a5767909c7d77e7d5
coyoteatento-debug <coyoteatento@gmail.com>
2026-05-28 22:29:38 -0600

    Update map_service.dart

--- lib/core/services/map_service.dart
+++ lib/core/services/map_service.dart
@@ -13,7 +13,7 @@ class MapService {
       'road-motorway': '#FF6500',
       'road-motorway-case': '#CC4E00',
       'road-motorway-link': '#FF6500',
-      'road-motorway-link-case': '#CC4E00',F
+      'road-motorway-link-case': '#CC4E00',
       'road-motorway-trunk': '#FF6500',
       'road-motorway-trunk-case': '#CC4E00',
       'road-trunk': '#FF6500',
